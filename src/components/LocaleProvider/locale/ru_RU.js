0ed4dac96d584643ebe622444f3a41dc05d51cde
melody.su@ucloud.cn <melody.su>
2024-03-28 15:44:29 +0800

    fix(language): update ru

---
+++ src/components/LocaleProvider/locale/ru_RU.js
@@ -0,0 +1,30 @@
+import Pagination from 'src/components/Pagination/locale/ru_RU';
+import DatePicker from 'src/components/DatePicker/locale/ru_RU';
+import Calendar from 'src/components/Calendar/locale/ru_RU';
+import Select from 'src/components/Select/locale/ru_RU';
+import Upload from 'src/components/Upload/locale/ru_RU';
+import Slider from 'src/components/Slider/locale/ru_RU';
+import Modal from 'src/components/Modal/locale/ru_RU';
+import Table from 'src/components/Table/locale/ru_RU';
+import Menu from 'src/components/Menu/locale/ru_RU';
+import PopConfirm from 'src/components/PopConfirm/locale/ru_RU';
+import EditableList from 'src/components/EditableList/locale/ru_RU';
+import Transfer from 'src/components/Transfer/locale/ru_RU';
+import SharedSearch from 'src/sharedComponents/Search/locale/ru_RU';
+
+export default {
+    locale: 'ru-ru',
+    Pagination,
+    DatePicker,
+    Calendar,
+    Select,
+    Upload,
+    Slider,
+    Modal,
+    Table,
+    Menu,
+    PopConfirm,
+    EditableList,
+    Transfer,
+    SharedSearch
+};
